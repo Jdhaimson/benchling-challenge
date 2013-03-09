47296848e83e71c7c56989f3240fb61ab14650e3
Josh Haimson <jdhaimson@gmail.com>
2013-03-09 15:40:35 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BenchlingChallenge::Application.config.secret_token = 'c32d3ce554f2e0a3acb4ee552cf419a0aa09e2b460f963da76c5ace3bd9ccc476087c198568c6f9d378bb22a63c4cb037b83544b8af6606073409bfcc1efe24b'
