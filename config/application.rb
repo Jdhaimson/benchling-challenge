de7290dc4abbcbe0357343e73d494aa8fcdf10af
Josh Haimson <jdhaimson@gmail.com>
2013-03-12 02:41:42 -0400

    Update config file for heroku

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module BenchlingChallenge
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #Hopefully get heroku to work
+    config.assets.initialize_on_precompile = false
   end
 end
